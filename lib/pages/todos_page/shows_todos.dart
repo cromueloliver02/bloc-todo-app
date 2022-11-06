697b8e8a8bcb20f91dd6eb41b836781f1dc521f9
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-07 00:48:39 +0800

    deleting and toggling complete todo items

--- lib/pages/todos_page/shows_todos.dart
+++ lib/pages/todos_page/shows_todos.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_todo_app/models/todo.dart';
 import 'package:flutter/material.dart';
 
 import '../../cubits/cubits.dart';
@@ -20,21 +21,68 @@ class ShowTodos extends StatelessWidget {
         return Dismissible(
           key: ValueKey(todo.id),
           direction: DismissDirection.startToEnd,
+          onDismissed: (direction) =>
+              ctx.read<TodoListCubit>().removeTodo(todo.id),
+          confirmDismiss: (direction) => showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (ctx) => AlertDialog(
+              title: const Text('Are you sure?'),
+              content: const Text('Do you really want to delete?'),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context, false),
+                  child: const Text('NO'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, true),
+                  child: const Text('YES'),
+                ),
+              ],
+            ),
+          ),
           background: Container(
             color: Colors.red,
             alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
             child: const Icon(
               Icons.delete,
               size: 30,
               color: Colors.white,
             ),
           ),
-          child: Text(
-            todo.desc,
-            style: const TextStyle(fontSize: 20),
-          ),
+          child: _TodoTile(todo: todo),
         );
       },
+    );
+  }
+}
+
+class _TodoTile extends StatefulWidget {
+  const _TodoTile({
+    Key? key,
+    required this.todo,
+  }) : super(key: key);
+
+  final Todo todo;
+
+  @override
+  State<_TodoTile> createState() => _TodoTileState();
+}
+
+class _TodoTileState extends State<_TodoTile> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        value: widget.todo.completed,
+        onChanged: (value) =>
+            context.read<TodoListCubit>().toggleComplete(widget.todo.id),
+      ),
+      title: Text(
+        widget.todo.desc,
+        style: const TextStyle(fontSize: 20),
+      ),
     );
   }
 }
