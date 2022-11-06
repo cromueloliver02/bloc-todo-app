8fa089938f6a15303b1227d46aadf1c88339fe22
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-06 22:06:24 +0800

    modularized todo header

--- lib/pages/todos_page/todo_header.dart
+++ lib/pages/todos_page/todo_header.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bloc_todo_app/cubits/cubits.dart';
 
-class TodosPage extends StatelessWidget {
-  const TodosPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 40,
-            ),
-            child: Column(
-              children: const [
-                TodoHeader(),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class TodoHeader extends StatelessWidget {
   const TodoHeader({super.key});
 
---
+++ lib/pages/todos_page/todos_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'todo_header.dart';
+
+class TodosPage extends StatelessWidget {
+  const TodosPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
+              children: const [
+                TodoHeader(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
