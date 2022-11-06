0b2729c700da413610ecab8a15421efe34aa16c9
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-06 21:59:54 +0800

    made items left count dynamic

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,14 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:bloc_todo_app/cubits/cubits.dart';
 
 class TodosPage extends StatelessWidget {
   const TodosPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
+              children: const [
+                TodoHeader(),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'TODO',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+        // BlocSelector<ActiveTodoCountCubit, ActiveTodoCountState, int>(
+        //   selector: (state) => state.activeTodoCount,
+        //   builder: (context, activeTodoCount) => Text(
+        //     '$activeTodoCount items left',
+        //     style: const TextStyle(
+        //       color: Colors.redAccent,
+        //       fontSize: 20,
+        //     ),
+        //   ),
+        // ),
+        Text(
+          '${context.watch<ActiveTodoCountCubit>().state.activeTodoCount} items left',
+          style: const TextStyle(
+            color: Colors.redAccent,
+            fontSize: 20,
+          ),
+        ),
+      ],
     );
   }
 }
